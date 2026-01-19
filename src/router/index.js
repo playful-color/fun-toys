9a1756f1126b64cbde5d4e8a721fe57066131392
shiba <shiba@imd-net.com>
2026-01-19 22:29:46 +0900

    #

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '@/pages/Home.vue'
 import About from '@/pages/About.vue'
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/fun-toys/'),
+  history: createWebHashHistory(),
   routes,
 })
 
