e1a0dc82f895af217f3c827c9dfb4e002e5859e8
shiba <shiba@imd-net.com>
2026-02-01 00:26:39 +0900

    ２月分のぬり絵に差し替え

--- src/components/Home/CanvasManager.vue
+++ src/components/Home/CanvasManager.vue
@@ -52,13 +52,12 @@ const emit = defineEmits([
 // ==================================================
 // 画像管理 -- useCharacterImageStore (ストア)　予定
 // ==================================================
-import pcA from '@/assets/images/home/01/2026_01a_pc.png';
-import pcB from '@/assets/images/home/01/2026_01b_pc.png';
-import pcC from '@/assets/images/home/01/2026_01c_pc.png';
-
-import spA from '@/assets/images/home/01/2026_01a_sp.png';
-import spB from '@/assets/images/home/01/2026_01b_sp.png';
-import spC from '@/assets/images/home/01/2026_01c_sp.png';
+import pcA from '@/assets/images/home/02/2026_02a_pc.png';
+import pcB from '@/assets/images/home/02/2026_02b_pc.png';
+import pcC from '@/assets/images/home/02/2026_02c_pc.png';
+import spA from '@/assets/images/home/02/2026_02a_sp.png';
+import spB from '@/assets/images/home/02/2026_02b_sp.png';
+import spC from '@/assets/images/home/02/2026_02c_sp.png';
 
 const pcImages = [pcA, pcB, pcC];
 const spImages = [spA, spB, spC];
