24ce10c14c3a9c34f9284745f4664cd3d3d3428c
shiba <shiba@imd-net.com>
2026-04-02 17:35:37 +0900

    4月塗り絵差し替え

--- src/data/characterImages.ts
+++ src/data/characterImages.ts
@@ -1,9 +1,9 @@
-import pcA from '@/assets/images/home/03/2026_03a_pc.png';
-import pcB from '@/assets/images/home/03/2026_03b_pc.png';
-import pcC from '@/assets/images/home/03/2026_03c_pc.png';
-import spA from '@/assets/images/home/03/2026_03a_sp.png';
-import spB from '@/assets/images/home/03/2026_03b_sp.png';
-import spC from '@/assets/images/home/03/2026_03c_sp.png';
+import pcA from '@/assets/images/home/04/2026_04a_pc.png';
+import pcB from '@/assets/images/home/04/2026_04b_pc.png';
+import pcC from '@/assets/images/home/04/2026_04c_pc.png';
+import spA from '@/assets/images/home/04/2026_04a_sp.png';
+import spB from '@/assets/images/home/04/2026_04b_sp.png';
+import spC from '@/assets/images/home/04/2026_04c_sp.png';
 
 // 文字列配列として明示的に型注釈
 export const pcImages: string[] = [pcA, pcB, pcC];
